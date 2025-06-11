1113459b48904580320a4ea4af31ba0dc8d4a76d
연수 정 <yunsu0224@kaywon.ac.kr>
2025-06-11 19:32:14 +0900

    fix: Correct all image paths to relative in beers.js

--- src/components/beers.js
+++ src/components/beers.js
@@ -11,7 +11,7 @@ const beerData = [
     feature: "가볍고 산뜻함",
     pairings: "치킨, 마른안주",
     description: "한국 대표 라거. 깔끔하고 청량해서 치킨과 궁합이 좋아요.",
-    imgsrc: "/img/korea/cass.png",
+    imgsrc: "./img/korea/cass.png",
     liked: false
     
   },
@@ -26,7 +26,7 @@ const beerData = [
     feature: "탄산감과 균형",
     pairings: "탕류, 두부요리",
     description: "밸런스 좋은 라거. 부드럽고 부담 없는 맛으로 사랑받아요.",
-    imgsrc: "/img/korea/hite.png",
+    imgsrc: "./img/korea/hite.png",
     liked: false
   },
   {
@@ -40,7 +40,7 @@ const beerData = [
     feature: "제주산 재료",
     pairings: "해산물, 과일 안주",
     description: "제주 감귤의 산뜻한 향과 밀맥의 부드러움이 조화를 이뤄요.",
-    imgsrc: "/img/korea/jeju.png",
+    imgsrc: "./img/korea/jeju.png",
     liked: false
   },
   {
@@ -54,7 +54,7 @@ const beerData = [
     feature: "무거운 바디",
     pairings: "육류, 바비큐",
     description: "독일식 원맥주 방식으로 진한 풍미를 자랑하는 라거.",
-    imgsrc: "/img/korea/kloud.png",
+    imgsrc: "./img/korea/kloud.png",
     liked: false
   },
   {
@@ -68,7 +68,7 @@ const beerData = [
     feature: "호주산 맥아 강조",
     pairings: "회, 해산물 요리",
     description: "청정 이미지와 깔끔한 맛으로 인기 있는 드라이 라거.",
-    imgsrc: "/img/korea/terra.png",
+    imgsrc: "./img/korea/terra.png",
     liked: false
   },
   {
@@ -82,7 +82,7 @@ const beerData = [
     feature: "드라이한 마무리",
     pairings: "초밥, 튀김",
     description: "일본식 드라이 라거의 대표주자. 깔끔한 마무리가 인상적.",
-    imgsrc: "/img/japan/asahi.png" ,
+    imgsrc: "./img/japan/asahi.png" ,
     liked: false
   },
   {
@@ -96,7 +96,7 @@ const beerData = [
     feature: "일본 최북단 홉 원산지",
     pairings: "라멘, 튀김",
     description: "홉의 쌉싸름함과 고소한 끝맛이 인상적인 전통 라거.",
-    imgsrc: "/img/japan/sapporo.png",
+    imgsrc: "./img/japan/sapporo.png",
     liked: false
   },
   {
@@ -110,7 +110,7 @@ const beerData = [
     feature: "첫 추출 맥아만 사용",
     pairings: "생선구이, 돈카츠",
     description: "첫 맥즙만을 사용한 깊고 부드러운 프리미엄 라거.",
-    imgsrc: "/img/japan/kirin.png",
+    imgsrc: "./img/japan/kirin.png",
     liked: false
   },
   {
@@ -124,7 +124,7 @@ const beerData = [
     feature: "일본 수제맥주 대표",
     pairings: "햄버거, 타코야키",
     description: "홉의 풍미가 살아 있는 일본 대표 크래프트 에일.",
-    imgsrc: "/img/japan/yona.png",
+    imgsrc: "./img/japan/yona.png",
     liked: false
   },
   {
@@ -138,7 +138,7 @@ const beerData = [
     feature: "고구마 원료의 독특함",
     pairings: "돈카츠, 구운 채소",
     description: "고구마를 이용한 일본만의 독창적 앰버 라거.",
-    imgsrc: "/img/japan/coedo.png",
+    imgsrc: "./img/japan/coedo.png",
     liked: false
   },
   {
@@ -152,7 +152,7 @@ const beerData = [
     feature: "뉴욕 대표 수제맥주",
     pairings: "버거, 감자튀김",
     description: "붉은빛 앰버 라거의 원조격 수제 맥주.",
-    imgsrc: "/img/usa/brooklyn.png",
+    imgsrc: "./img/usa/brooklyn.png",
     liked: false
   },
   {
@@ -166,7 +166,7 @@ const beerData = [
     feature: "대표적인 미국 IPA",
     pairings: "스파이시 치킨, 피자",
     description: "홉의 향이 풍부하게 터지는 정통 IPA 스타일.",
-    imgsrc: "/img/usa/goose.png",
+    imgsrc: "./img/usa/goose.png",
     liked: false
   },
   {
@@ -180,7 +180,7 @@ const beerData = [
     feature: "크리미한 질감, 오렌지 슬라이스와 함께",
     pairings: "샐러드, 크림 파스타",
     description: "부드럽고 향긋한 미국식 밀맥주. 오렌지와 함께 즐기면 더 좋아요.",
-    imgsrc: "/img/usa/bulemoon.png",
+    imgsrc: "./img/usa/bulemoon.png",
     liked: false
   },
   {
@@ -194,7 +194,7 @@ const beerData = [
     feature: "미국 서부 IPA 대표 브랜드",
     pairings: "버팔로윙, 맥앤치즈",
     description: "홉의 쌉싸름한 맛이 진하게 퍼지는 정통 미국 IPA.",
-    imgsrc: "/img/usa/ipa.png",
+    imgsrc: "./img/usa/ipa.png",
     liked: false
   },
   {
@@ -208,7 +208,7 @@ const beerData = [
     feature: "가볍게 마시기 좋음",
     pairings: "프레첼, 그릴드 소시지",
     description: "독일식 정통 라거. 부담 없이 마시기 좋아요.",
-    imgsrc: "/img/germany/becks.png",
+    imgsrc: "./img/germany/becks.png",
     liked: false
   },
   {
@@ -222,7 +222,7 @@ const beerData = [
     feature: "세계에서 가장 오래된 양조장",
     pairings: "슈니첼, 감자요리",
     description: "독일 바이에른 전통을 대표하는 부드러운 헬레스 라거.",
-    imgsrc: "/img/germany/Weihenstephaner.png",
+    imgsrc: "./img/germany/Weihenstephaner.png",
     liked: false
   },
   
@@ -238,7 +238,7 @@ const beerData = [
     feature: "풍부한 거품과 청량함",
     pairings: "샐러드, 과일 디저트",
     description: "과일향이 가득한 독일 밀맥주의 정석!",
-    imgsrc: "/img/germany/Paulaner.png",
+    imgsrc: "./img/germany/Paulaner.png",
     liked: false
   },
   {
@@ -252,7 +252,7 @@ const beerData = [
     feature: "벨기에 대표 밀맥주",
     pairings: "해산물, 샐러드",
     description: "오렌지 껍질과 고수 씨의 향긋함이 매력적인 밀맥주.",
-    imgsrc: "/img/belgium/hoegaarden.png",
+    imgsrc: "./img/belgium/hoegaarden.png",
     liked: false
   },
   {
@@ -266,7 +266,7 @@ const beerData = [
     feature: "풍부한 향과 바디감",
     pairings: "치즈, 로스트 치킨",
     description: "전통 수도원 스타일의 깊은 풍미가 느껴지는 에일.",
-    imgsrc: "/img/belgium/leffe.png",
+    imgsrc: "./img/belgium/leffe.png",
     liked: false
   },
   {
@@ -280,7 +280,7 @@ const beerData = [
     feature: "고도수 트라피스트 맥주",
     pairings: "진한 스튜, 숙성 치즈",
     description: "깊고 복합적인 풍미가 일품인 벨기에 수도원 맥주.",
-    imgsrc: "/img/belgium/chimay.png",
+    imgsrc: "./img/belgium/chimay.png",
     liked: false
   },
   {
@@ -294,7 +294,7 @@ const beerData = [
     feature: "강한 탄산과 드라이함",
     pairings: "생선요리, 해산물 파스타",
     description: "강한 탄산과 드라이한 바디가 매력적인 고도수 에일.",
-    imgsrc: "/img/belgium/duvel.png",
+    imgsrc: "./img/belgium/duvel.png",
     liked: false
   },
   {
@@ -308,7 +308,7 @@ const beerData = [
     feature: "쾌활한 느낌의 강한 에일",
     pairings: "매콤한 볶음요리, 햄",
     description: "귀여운 난장이 캐릭터로 유명한 향긋한 스트롱 에일.",
-    imgsrc: "/img/belgium/lachouffe.png",
+    imgsrc: "./img/belgium/lachouffe.png",
     liked: false
   },
   {
@@ -322,7 +322,7 @@ const beerData = [
     feature: "트라피스트 최고 강도",
     pairings: "치즈 플래터, 초콜릿",
     description: "풍부하고 진한 맛의 최고급 트라피스트 맥주.",
-    imgsrc: "/img/belgium/rochefort.png",
+    imgsrc: "./img/belgium/rochefort.png",
     liked: false
   }
 ];
